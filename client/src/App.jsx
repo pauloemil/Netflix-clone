1d552658ff4b09c143b9cf113dbd84b6da0a6fb1
pauloemil <prog.paulo@hotmail.com>
2021-09-07 13:49:51 +0200

    register page creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import "./app.scss";
-import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
 
 const App = () => {
-  return <Watch />;
+  return <Register />;
 };
 
 export default App;
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,64 @@
-import ".Register.scss";
+import "./Register.scss";
+import { useState } from "react";
 
 const Register = () => {
-  return <div></div>;
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [started, setstarted] = useState(false);
+  const submitHandle = () => {};
+  return (
+    <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt="logo"
+            className="logo"
+          />
+          <button className="loginButton">Sign In</button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {started ? (
+          <>
+            <div className="input">
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setpassword(e.target.value)}
+              />
+              <button className="registerButton" onClick={() => submitHandle()}>
+                Start
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="input">
+              <input
+                type="email"
+                placeholder="Email Adress"
+                value={email}
+                onChange={(e) => setemail(e.target.value)}
+              />
+              <button
+                className="registerButton"
+                onClick={() => setstarted(true)}
+              >
+                Get Started
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Register;
