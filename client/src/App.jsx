a9923a1d57b74b4497cb57e9b42a1c14d788e658
pauloemil <prog.paulo@hotmail.com>
2021-09-08 09:13:42 +0200

    setting server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,31 @@
 import "./app.scss";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return <Login />;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/watch">
+            <Watch />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
