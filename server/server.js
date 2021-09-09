02816d14b37db4c591fda6ed690a85ed2eb55c29
pauloemil <prog.paulo@hotmail.com>
2021-09-09 16:59:31 +0200

    adding routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
 const movieRouter = require("./routes/movies");
+const listRouter = require("./routes/lists");
 const PORT = process.env.PORT || 3000;
 require("./config/database");
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/movie", movieRouter);
+app.use("/api/list", listRouter);
 app.listen(PORT, () => {
   console.log("http://localhost:" + PORT);
 });
