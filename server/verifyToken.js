12a688914823555acb329b56152debad95c23df2
pauloemil <prog.paulo@hotmail.com>
2021-09-09 14:32:31 +0200

    verification middlewhere

---
+++ server/verifyToken.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const verify = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(" ")[1];
+  //   console.log(token);
+  if (token) {
+    jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, user) => {
+      if (err) res.json({ authintication: err.message });
+      else {
+        // console.log(user);
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+module.exports = verify;
