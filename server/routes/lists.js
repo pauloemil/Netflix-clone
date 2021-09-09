f95cc6cbd3e34cc4c87cc04660386d7783ca5f83
pauloemil <prog.paulo@hotmail.com>
2021-09-09 16:59:03 +0200

    create lists route

---
+++ server/routes/lists.js
@@ -0,0 +1,56 @@
+const Router = require("express").Router();
+const List = require("../models/List");
+const verifyToken = require("../verifyToken");
+
+// create
+Router.post("/", verifyToken, (req, res) => {
+  if (req.user.isAdmin) {
+    List.create(req.body, (err, list) => {
+      if (err) res.json({ listRoutCreate1: err.message });
+      else res.json(list);
+    });
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+// delete
+Router.delete("/:id", verifyToken, (req, res) => {
+  if (req.user.isAdmin) {
+    List.findByIdAndDelete(req.params.id, (err, deletedList) => {
+      if (err) res.json({ movieRouteDELETE: err.message });
+      else if (!deletedList) res.sendStatus(404);
+      else res.sendStatus(200);
+    });
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+// get
+Router.get("/", verifyToken, (req, res) => {
+  const typeQ = req.query.type;
+  const genreQ = req.query.genre;
+
+  List.aggregate(
+    [
+      { $sample: { size: 10 } },
+      {
+        $match:
+          typeQ && genreQ
+            ? { type: typeQuery, genre: genreQuery }
+            : typeQ
+            ? { type: typeQuery }
+            : genreQ
+            ? { genre: genreQuery }
+            : {},
+      },
+    ],
+    (err, lists) => {
+      if (err) res.json({ movieRouteGET: err.message });
+      else res.json(lists);
+    }
+  );
+});
+
+module.exports = Router;
