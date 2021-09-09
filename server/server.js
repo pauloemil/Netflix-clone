403d643dbb54a366b966348e83f9e9c2b6f84e8a
pauloemil <prog.paulo@hotmail.com>
2021-09-09 14:32:42 +0200

    adding routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-require("dotenv").config();
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/users");
+const movieRouter = require("./routes/movies");
 const PORT = process.env.PORT || 3000;
 require("./config/database");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routing & Routes
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/movie", movieRouter);
 app.listen(PORT, () => {
   console.log("http://localhost:" + PORT);
 });
